6208b7170b807123b677382ad2639e670a6579a5
alistvt <alistvt@gmail.com>
2019-01-14 04:21:00 +0330

    some templatings - templates almost are done

--- tasks/forms.py
+++ tasks/forms.py
@@ -1,5 +1,11 @@
 from django import forms
+from .models import Task
 
 class LoginForm(forms.Form):
     email = forms.EmailField()
     password = forms.CharField(widget=forms.PasswordInput)
+
+class TaskForm(forms.ModelForm):
+    class Meta:
+        model = Task
+        fields = ('title', 'group', 'user', )
